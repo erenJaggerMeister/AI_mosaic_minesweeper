03df02981b0520e5ab4153ff094153bb37e23ed7
Marcellius <123653014+erenJaggerMeister@users.noreply.github.com>
2023-12-22 00:30:39 +0700

    perubahan pada pembacaan input

--- tbIndividual.java
+++ tbIndividual.java
@@ -2,14 +2,19 @@ public class tbIndividual {
     private final int[] chromosome;
     private double fitness = -1;
 
+    // TODO!!!
+    private long seed;
+
     public tbIndividual(int[] chromosome) {
         this.chromosome = chromosome;
     }
 
-    public tbIndividual(int chromosomeLength) {
+    public tbIndividual(int chromosomeLength, long iptSeed) {
         this.chromosome = new int[chromosomeLength];
+        this.seed = iptSeed;
+        tbRandomGenerator rand = new tbRandomGenerator(iptSeed);
         for (int gene = 0; gene < chromosomeLength; gene++) {
-            if (0.5 < Math.random()) {
+            if (0.5 < rand.getRandom()) {
                 this.setGene(gene, 1);
             } else {
                 this.setGene(gene, 0);
@@ -59,7 +64,7 @@ public class tbIndividual {
         StringBuilder output = new StringBuilder();
         for (int gene_1 = 0; gene_1 < sqrt; gene_1++) {
             for (int gene_2 = 0; gene_2 < sqrt; gene_2++) {
-                output.append(this.chromosome[gene_1*sqrt+gene_2]).append(" ");
+                output.append(this.chromosome[gene_1 * sqrt + gene_2]).append(" ");
             }
             output.append("\n");
         }
--- tbPopulation.java
+++ tbPopulation.java
@@ -5,14 +5,19 @@ public class tbPopulation {
     private tbIndividual population[];
     private double populationFitness = -1;
 
+    // TODO!!!
+    // private tbRandomGenerator rand;
+    private long seed;
+
     public tbPopulation(int populationSize) {
         this.population = new tbIndividual[populationSize];
     }
 
-    public tbPopulation(int populationSize, int chromosomeLength) {
+    public tbPopulation(int populationSize, int chromosomeLength, long iptSeed) {
         this.population = new tbIndividual[populationSize];
+        this.seed = iptSeed;
         for (int individualCount = 0; individualCount < populationSize; individualCount++) {
-            tbIndividual individual = new tbIndividual(chromosomeLength);
+            tbIndividual individual = new tbIndividual(chromosomeLength, this.seed);
             this.population[individualCount] = individual;
         }
     }
@@ -22,22 +27,22 @@ public class tbPopulation {
     }
 
     public tbIndividual getFittes(int offset) {
-         Arrays.sort(this.population, new Comparator<tbIndividual>() {
-         @Override
-             public int compare(tbIndividual o1, tbIndividual o2) {
-                 if (o1.getFitness() > o2.getFitness()) {
+        Arrays.sort(this.population, new Comparator<tbIndividual>() {
+            @Override
+            public int compare(tbIndividual o1, tbIndividual o2) {
+                if (o1.getFitness() > o2.getFitness()) {
                     return -1;
-                 } else if (o1.getFitness() < o2.getFitness()) {
+                } else if (o1.getFitness() < o2.getFitness()) {
                     return 1;
-                 }
-                 return 0;
-             }
-         });
-//        System.out.println("INI PRINT DARI GETFITTES");
-//        for (int i = 0; i < this.population.length; i++) {
-//            System.out.println(i + " :");
-//            System.out.println(getIndividual(i));
-//        }
+                }
+                return 0;
+            }
+        });
+        // System.out.println("INI PRINT DARI GETFITTES");
+        // for (int i = 0; i < this.population.length; i++) {
+        // System.out.println(i + " :");
+        // System.out.println(getIndividual(i));
+        // }
         return this.population[offset];
     }
 
